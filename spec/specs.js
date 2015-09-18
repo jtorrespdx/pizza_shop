0522572fabf2ba70cef2d46b17964f630dc6c632
jose <jtorres.pdx@gmail.com>
2015-09-18 14:28:02 -0700

    add first function. Construct pizza.

--- spec/specs.js
+++ spec/specs.js
@@ -2,10 +2,11 @@ describe('pizza', function() {
   it("creates a new pizza item with its properties", function() {
     var quantity = 1;
     var pizzaSize = ["small", 8];
-    var pizzaToppings - ["cheese, pepperoni"];
-    var testPizza = new Pizza(quantity, pizzaSize. pizzaToppings);
+    var pizzaToppings = ["cheese",  "pepperoni"];
+    var testPizza = new Pizza(quantity, pizzaSize, pizzaToppings);
     expect(testPizza.quantity).to.equal(1);
     expect(testPizza.pizzaSize).to.eql(["small", 8]);
-    expect(testPizza.).to.eql.(["cheese", "pepperoni"]);
+    expect(testPizza.pizzaToppings).to.eql(["cheese", "pepperoni"]);
   });
+
 });
